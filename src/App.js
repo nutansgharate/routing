b1391d3b93a411ff0f925c006efaf4c211f56612
gharate nutan <gharatenutan1@gmail.com>
2021-01-21 09:41:55 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+ 
+function App() {
+  // LOGIC
+  // Javascript
+
+  // WRTIER HTML HERE
+  return (
+    <div>
+      <Home />
+      <Page1 />
+
+      <br />
+      <Page1 />
+
+      <br />
+      <Page1 />
+
+      <br />
+      <Page1 />
+    </div>
+  );
+}
+
+export default App;
+
