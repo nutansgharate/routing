575038b4fafb6539fd9757b02e7a03c4c20fc840
gharate nutan <gharatenutan1@gmail.com>
2021-01-21 09:51:24 +0000

    main

--- src/App.js
+++ src/App.js
@@ -2,29 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
- 
+/**
+ * http://localhost:3000/
+ * http://localhost:3000/page1
+ * http://localhost:3000/page2
+ * http://localhost:3000/page3
+ */
 function App() {
-  // LOGIC
-  // Javascript
-
-  // WRTIER HTML HERE
   return (
-    <div>
-      <Home />
-      <Page1 />
+    <Router>
+      <Route exact path="/">
+        <div>Default Page</div>
+      </Route>
 
-      <br />
-      <Page1 />
+      <Route path="/page1">
+        <div>Page 1</div>
+      </Route>
 
-      <br />
-      <Page1 />
+      <Route path="/page2">
+        <div>Page 2</div>
+      </Route>
 
-      <br />
-      <Page1 />
-    </div>
+      <Route path="/page3">
+        <div>Page 3</div>
+      </Route>
+    </Router>
   );
 }
 
 export default App;
-
